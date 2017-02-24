20bce3613a753aceda6f1e34aec1b1518f2d7124
Kentaro Matsumae <kentaro.matsumae@dena.com>
2017-02-24 14:33:37 +0900

    fix for tf 1.0

--- main/model.py
+++ main/model.py
@@ -214,166 +214,168 @@ class pix2pix(object):
 
     def discriminator(self, image, y=None, reuse=False):
         # image is 256 x 256 x (input_c_dim + output_c_dim)
-        if reuse:
-            tf.get_variable_scope().reuse_variables()
-        else:
-            assert tf.get_variable_scope().reuse == False
+        with tf.variable_scope("discriminator") as scope:
+            if reuse:
+                scope.reuse_variables()
+            else:
+                assert tf.get_variable_scope().reuse == False
 
-        h0 = lrelu(conv2d(image, self.df_dim, name='d_h0_conv'))
-        # h0 is (128 x 128 x self.df_dim)
-        h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim*2, name='d_h1_conv')))
-        # h1 is (64 x 64 x self.df_dim*2)
-        h2 = lrelu(self.d_bn2(conv2d(h1, self.df_dim*4, name='d_h2_conv')))
-        # h2 is (32x 32 x self.df_dim*4)
-        h3 = lrelu(self.d_bn3(conv2d(h2, self.df_dim*8, d_h=1, d_w=1, name='d_h3_conv')))
-        # h3 is (16 x 16 x self.df_dim*8)
-        h4 = linear(tf.reshape(h3, [self.batch_size, -1]), 1, 'd_h3_lin')
+            h0 = lrelu(conv2d(image, self.df_dim, name='d_h0_conv'))
+            # h0 is (128 x 128 x self.df_dim)
+            h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim*2, name='d_h1_conv')))
+            # h1 is (64 x 64 x self.df_dim*2)
+            h2 = lrelu(self.d_bn2(conv2d(h1, self.df_dim*4, name='d_h2_conv')))
+            # h2 is (32x 32 x self.df_dim*4)
+            h3 = lrelu(self.d_bn3(conv2d(h2, self.df_dim*8, d_h=1, d_w=1, name='d_h3_conv')))
+            # h3 is (16 x 16 x self.df_dim*8)
+            h4 = linear(tf.reshape(h3, [self.batch_size, -1]), 1, 'd_h3_lin')
 
-        return tf.nn.sigmoid(h4), h4
+            return tf.nn.sigmoid(h4), h4
 
     def generator(self, image, y=None):
         #print(image.get_shape) #(1, 384, 256, 3)
+        with tf.variable_scope("generator") as scope:
+            s = self.output_size #[384,256]
+            s2, s4, s8, s16, s32, s64, s128 = s/2, s/4, s/8, s/16, s/32, s/64, s/128
 
-        s = self.output_size #[384,256]
-        s2, s4, s8, s16, s32, s64, s128 = s/2, s/4, s/8, s/16, s/32, s/64, s/128
+            # image is (384 x 256 x input_c_dim)
+            e1 = conv2d(image, self.gf_dim, name='g_e1_conv')
+            # e1 is (192 x 128 x self.gf_dim)
+            e2 = self.g_bn_e2(conv2d(lrelu(e1), self.gf_dim*2, name='g_e2_conv'))
+            # e2 is (96 x 64 x self.gf_dim*2)
+            e3 = self.g_bn_e3(conv2d(lrelu(e2), self.gf_dim*4, name='g_e3_conv'))
+            # e3 is (48 x 32 x self.gf_dim*4)
+            e4 = self.g_bn_e4(conv2d(lrelu(e3), self.gf_dim*8, name='g_e4_conv'))
+            # e4 is (24 x 16 x self.gf_dim*8)
+            e5 = self.g_bn_e5(conv2d(lrelu(e4), self.gf_dim*8, name='g_e5_conv'))
+            # e5 is (12 x 8 x self.gf_dim*8)
+            e6 = self.g_bn_e6(conv2d(lrelu(e5), self.gf_dim*8, name='g_e6_conv'))
+            # e6 is (6 x 4 x self.gf_dim*8)
+            e7 = self.g_bn_e7(conv2d(lrelu(e6), self.gf_dim*8, name='g_e7_conv'))
+            # e7 is (3 x 2 x self.gf_dim*8)
+            e8 = self.g_bn_e8(conv2d(lrelu(e7), self.gf_dim*8, name='g_e8_conv'))
+            # e8 is (2 x 1 x self.gf_dim*8)
 
-        # image is (384 x 256 x input_c_dim)
-        e1 = conv2d(image, self.gf_dim, name='g_e1_conv')
-        # e1 is (192 x 128 x self.gf_dim)
-        e2 = self.g_bn_e2(conv2d(lrelu(e1), self.gf_dim*2, name='g_e2_conv'))
-        # e2 is (96 x 64 x self.gf_dim*2)
-        e3 = self.g_bn_e3(conv2d(lrelu(e2), self.gf_dim*4, name='g_e3_conv'))
-        # e3 is (48 x 32 x self.gf_dim*4)
-        e4 = self.g_bn_e4(conv2d(lrelu(e3), self.gf_dim*8, name='g_e4_conv'))
-        # e4 is (24 x 16 x self.gf_dim*8)
-        e5 = self.g_bn_e5(conv2d(lrelu(e4), self.gf_dim*8, name='g_e5_conv'))
-        # e5 is (12 x 8 x self.gf_dim*8)
-        e6 = self.g_bn_e6(conv2d(lrelu(e5), self.gf_dim*8, name='g_e6_conv'))
-        # e6 is (6 x 4 x self.gf_dim*8)
-        e7 = self.g_bn_e7(conv2d(lrelu(e6), self.gf_dim*8, name='g_e7_conv'))
-        # e7 is (3 x 2 x self.gf_dim*8)
-        e8 = self.g_bn_e8(conv2d(lrelu(e7), self.gf_dim*8, name='g_e8_conv'))
-        # e8 is (2 x 1 x self.gf_dim*8)
+            self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
+                [self.batch_size, int(s128[0]), int(s128[1]), self.gf_dim*8], name='g_d1', with_w=True)
+            d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
+            d1 = tf.concat(axis=3, values=[d1, e7])
+            # d1 is (2 x 2 x self.gf_dim*8*2)
 
-        self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
-            [self.batch_size, int(s128[0]), int(s128[1]), self.gf_dim*8], name='g_d1', with_w=True)
-        d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
-        d1 = tf.concat(axis=3, values=[d1, e7])
-        # d1 is (2 x 2 x self.gf_dim*8*2)
+            self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
+                [self.batch_size, int(s64[0]), int(s64[1]), self.gf_dim*8], name='g_d2', with_w=True)
+            d2 = tf.nn.dropout(self.g_bn_d2(self.d2), 0.5)
+            d2 = tf.concat(axis=3, values=[d2, e6])
+            # d2 is (4 x 4 x self.gf_dim*8*2)
 
-        self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
-            [self.batch_size, int(s64[0]), int(s64[1]), self.gf_dim*8], name='g_d2', with_w=True)
-        d2 = tf.nn.dropout(self.g_bn_d2(self.d2), 0.5)
-        d2 = tf.concat(axis=3, values=[d2, e6])
-        # d2 is (4 x 4 x self.gf_dim*8*2)
+            self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
+                [self.batch_size, int(s32[0]), int(s32[1]), self.gf_dim*8], name='g_d3', with_w=True)
+            d3 = tf.nn.dropout(self.g_bn_d3(self.d3), 0.5)
+            d3 = tf.concat(axis=3, values=[d3, e5])
+            # d3 is (8 x 8 x self.gf_dim*8*2)
 
-        self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
-            [self.batch_size, int(s32[0]), int(s32[1]), self.gf_dim*8], name='g_d3', with_w=True)
-        d3 = tf.nn.dropout(self.g_bn_d3(self.d3), 0.5)
-        d3 = tf.concat(axis=3, values=[d3, e5])
-        # d3 is (8 x 8 x self.gf_dim*8*2)
+            self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
+                [self.batch_size, int(s16[0]), int(s16[1]), self.gf_dim*8], name='g_d4', with_w=True)
+            d4 = self.g_bn_d4(self.d4)
+            d4 = tf.concat(axis=3, values=[d4, e4])
+            # d4 is (16 x 16 x self.gf_dim*8*2)
 
-        self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
-            [self.batch_size, int(s16[0]), int(s16[1]), self.gf_dim*8], name='g_d4', with_w=True)
-        d4 = self.g_bn_d4(self.d4)
-        d4 = tf.concat(axis=3, values=[d4, e4])
-        # d4 is (16 x 16 x self.gf_dim*8*2)
+            self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
+                [self.batch_size, int(s8[0]), int(s8[1]), self.gf_dim*4], name='g_d5', with_w=True)
+            d5 = self.g_bn_d5(self.d5)
+            d5 = tf.concat(axis=3, values=[d5, e3])
+            # d5 is (32 x 32 x self.gf_dim*4*2)
 
-        self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
-            [self.batch_size, int(s8[0]), int(s8[1]), self.gf_dim*4], name='g_d5', with_w=True)
-        d5 = self.g_bn_d5(self.d5)
-        d5 = tf.concat(axis=3, values=[d5, e3])
-        # d5 is (32 x 32 x self.gf_dim*4*2)
+            self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
+                [self.batch_size, int(s4[0]), int(s4[1]), self.gf_dim*2], name='g_d6', with_w=True)
+            d6 = self.g_bn_d6(self.d6)
+            d6 = tf.concat(axis=3, values=[d6, e2])
+            # d6 is (64 x 64 x self.gf_dim*2*2)
 
-        self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
-            [self.batch_size, int(s4[0]), int(s4[1]), self.gf_dim*2], name='g_d6', with_w=True)
-        d6 = self.g_bn_d6(self.d6)
-        d6 = tf.concat(axis=3, values=[d6, e2])
-        # d6 is (64 x 64 x self.gf_dim*2*2)
+            self.d7, self.d7_w, self.d7_b = deconv2d(tf.nn.relu(d6),
+                [self.batch_size, int(s2[0]), int(s2[1]), self.gf_dim], name='g_d7', with_w=True)
+            d7 = self.g_bn_d7(self.d7)
+            d7 = tf.concat(axis=3, values=[d7, e1])
+            # d7 is (128 x 128 x self.gf_dim*1*2)
 
-        self.d7, self.d7_w, self.d7_b = deconv2d(tf.nn.relu(d6),
-            [self.batch_size, int(s2[0]), int(s2[1]), self.gf_dim], name='g_d7', with_w=True)
-        d7 = self.g_bn_d7(self.d7)
-        d7 = tf.concat(axis=3, values=[d7, e1])
-        # d7 is (128 x 128 x self.gf_dim*1*2)
+            self.d8, self.d8_w, self.d8_b = deconv2d(tf.nn.relu(d7),
+                [self.batch_size, int(s[0]), int(s[1]), self.output_c_dim], name='g_d8', with_w=True)
+            # d8 is (256 x 256 x output_c_dim)
 
-        self.d8, self.d8_w, self.d8_b = deconv2d(tf.nn.relu(d7),
-            [self.batch_size, int(s[0]), int(s[1]), self.output_c_dim], name='g_d8', with_w=True)
-        # d8 is (256 x 256 x output_c_dim)
-
-        return tf.nn.tanh(self.d8)
+            return tf.nn.tanh(self.d8)
 
     def sampler(self, image, y=None):
-        tf.get_variable_scope().reuse_variables()
+        with tf.variable_scope("generator") as scope:
+            scope.reuse_variables()
 
-        s = self.output_size
-        s2, s4, s8, s16, s32, s64, s128 = s/2, s/4, s/8, s/16, s/32, s/64, s/128
+            s = self.output_size
+            s2, s4, s8, s16, s32, s64, s128 = s/2, s/4, s/8, s/16, s/32, s/64, s/128
 
-        # image is (256 x 256 x input_c_dim)
-        e1 = conv2d(image, self.gf_dim, name='g_e1_conv')
-        # e1 is (128 x 128 x self.gf_dim)
-        e2 = self.g_bn_e2(conv2d(lrelu(e1), self.gf_dim*2, name='g_e2_conv'))
-        # e2 is (64 x 64 x self.gf_dim*2)
-        e3 = self.g_bn_e3(conv2d(lrelu(e2), self.gf_dim*4, name='g_e3_conv'))
-        # e3 is (32 x 32 x self.gf_dim*4)
-        e4 = self.g_bn_e4(conv2d(lrelu(e3), self.gf_dim*8, name='g_e4_conv'))
-        # e4 is (16 x 16 x self.gf_dim*8)
-        e5 = self.g_bn_e5(conv2d(lrelu(e4), self.gf_dim*8, name='g_e5_conv'))
-        # e5 is (8 x 8 x self.gf_dim*8)
-        e6 = self.g_bn_e6(conv2d(lrelu(e5), self.gf_dim*8, name='g_e6_conv'))
-        # e6 is (4 x 4 x self.gf_dim*8)
-        e7 = self.g_bn_e7(conv2d(lrelu(e6), self.gf_dim*8, name='g_e7_conv'))
-        # e7 is (2 x 2 x self.gf_dim*8)
-        e8 = self.g_bn_e8(conv2d(lrelu(e7), self.gf_dim*8, name='g_e8_conv'))
-        # e8 is (1 x 1 x self.gf_dim*8)
+            # image is (256 x 256 x input_c_dim)
+            e1 = conv2d(image, self.gf_dim, name='g_e1_conv')
+            # e1 is (128 x 128 x self.gf_dim)
+            e2 = self.g_bn_e2(conv2d(lrelu(e1), self.gf_dim*2, name='g_e2_conv'))
+            # e2 is (64 x 64 x self.gf_dim*2)
+            e3 = self.g_bn_e3(conv2d(lrelu(e2), self.gf_dim*4, name='g_e3_conv'))
+            # e3 is (32 x 32 x self.gf_dim*4)
+            e4 = self.g_bn_e4(conv2d(lrelu(e3), self.gf_dim*8, name='g_e4_conv'))
+            # e4 is (16 x 16 x self.gf_dim*8)
+            e5 = self.g_bn_e5(conv2d(lrelu(e4), self.gf_dim*8, name='g_e5_conv'))
+            # e5 is (8 x 8 x self.gf_dim*8)
+            e6 = self.g_bn_e6(conv2d(lrelu(e5), self.gf_dim*8, name='g_e6_conv'))
+            # e6 is (4 x 4 x self.gf_dim*8)
+            e7 = self.g_bn_e7(conv2d(lrelu(e6), self.gf_dim*8, name='g_e7_conv'))
+            # e7 is (2 x 2 x self.gf_dim*8)
+            e8 = self.g_bn_e8(conv2d(lrelu(e7), self.gf_dim*8, name='g_e8_conv'))
+            # e8 is (1 x 1 x self.gf_dim*8)
 
-        self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
-            [self.batch_size, int(s128[0]), int(s128[1]), self.gf_dim*8], name='g_d1', with_w=True)
-        d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
-        d1 = tf.concat(axis=3, values=[d1, e7])
-        # d1 is (2 x 2 x self.gf_dim*8*2)
+            self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
+                [self.batch_size, int(s128[0]), int(s128[1]), self.gf_dim*8], name='g_d1', with_w=True)
+            d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
+            d1 = tf.concat(axis=3, values=[d1, e7])
+            # d1 is (2 x 2 x self.gf_dim*8*2)
 
-        self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
-            [self.batch_size, int(s64[0]), int(s64[1]), self.gf_dim*8], name='g_d2', with_w=True)
-        d2 = tf.nn.dropout(self.g_bn_d2(self.d2), 0.5)
-        d2 = tf.concat(axis=3, values=[d2, e6])
-        # d2 is (4 x 4 x self.gf_dim*8*2)
+            self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
+                [self.batch_size, int(s64[0]), int(s64[1]), self.gf_dim*8], name='g_d2', with_w=True)
+            d2 = tf.nn.dropout(self.g_bn_d2(self.d2), 0.5)
+            d2 = tf.concat(axis=3, values=[d2, e6])
+            # d2 is (4 x 4 x self.gf_dim*8*2)
 
-        self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
-            [self.batch_size, int(s32[0]), int(s32[1]), self.gf_dim*8], name='g_d3', with_w=True)
-        d3 = tf.nn.dropout(self.g_bn_d3(self.d3), 0.5)
-        d3 = tf.concat(axis=3, values=[d3, e5])
-        # d3 is (8 x 8 x self.gf_dim*8*2)
+            self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
+                [self.batch_size, int(s32[0]), int(s32[1]), self.gf_dim*8], name='g_d3', with_w=True)
+            d3 = tf.nn.dropout(self.g_bn_d3(self.d3), 0.5)
+            d3 = tf.concat(axis=3, values=[d3, e5])
+            # d3 is (8 x 8 x self.gf_dim*8*2)
 
-        self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
-            [self.batch_size, int(s16[0]), int(s16[1]), self.gf_dim*8], name='g_d4', with_w=True)
-        d4 = self.g_bn_d4(self.d4)
-        d4 = tf.concat(axis=3, values=[d4, e4])
-        # d4 is (16 x 16 x self.gf_dim*8*2)
+            self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
+                [self.batch_size, int(s16[0]), int(s16[1]), self.gf_dim*8], name='g_d4', with_w=True)
+            d4 = self.g_bn_d4(self.d4)
+            d4 = tf.concat(axis=3, values=[d4, e4])
+            # d4 is (16 x 16 x self.gf_dim*8*2)
 
-        self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
-            [self.batch_size, int(s8[0]), int(s8[1]), self.gf_dim*4], name='g_d5', with_w=True)
-        d5 = self.g_bn_d5(self.d5)
-        d5 = tf.concat(axis=3, values=[d5, e3])
-        # d5 is (32 x 32 x self.gf_dim*4*2)
+            self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
+                [self.batch_size, int(s8[0]), int(s8[1]), self.gf_dim*4], name='g_d5', with_w=True)
+            d5 = self.g_bn_d5(self.d5)
+            d5 = tf.concat(axis=3, values=[d5, e3])
+            # d5 is (32 x 32 x self.gf_dim*4*2)
 
-        self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
-            [self.batch_size, int(s4[0]), int(s4[1]), self.gf_dim*2], name='g_d6', with_w=True)
-        d6 = self.g_bn_d6(self.d6)
-        d6 = tf.concat(axis=3, values=[d6, e2])
-        # d6 is (64 x 64 x self.gf_dim*2*2)
+            self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
+                [self.batch_size, int(s4[0]), int(s4[1]), self.gf_dim*2], name='g_d6', with_w=True)
+            d6 = self.g_bn_d6(self.d6)
+            d6 = tf.concat(axis=3, values=[d6, e2])
+            # d6 is (64 x 64 x self.gf_dim*2*2)
 
-        self.d7, self.d7_w, self.d7_b = deconv2d(tf.nn.relu(d6),
-            [self.batch_size, int(s2[0]), int(s2[1]), self.gf_dim], name='g_d7', with_w=True)
-        d7 = self.g_bn_d7(self.d7)
-        d7 = tf.concat(axis=3, values=[d7, e1])
-        # d7 is (128 x 128 x self.gf_dim*1*2)
+            self.d7, self.d7_w, self.d7_b = deconv2d(tf.nn.relu(d6),
+                [self.batch_size, int(s2[0]), int(s2[1]), self.gf_dim], name='g_d7', with_w=True)
+            d7 = self.g_bn_d7(self.d7)
+            d7 = tf.concat(axis=3, values=[d7, e1])
+            # d7 is (128 x 128 x self.gf_dim*1*2)
 
-        self.d8, self.d8_w, self.d8_b = deconv2d(tf.nn.relu(d7),
-            [self.batch_size, int(s[0]), int(s[1]), self.output_c_dim], name='g_d8', with_w=True)
-        # d8 is (256 x 256 x output_c_dim)
+            self.d8, self.d8_w, self.d8_b = deconv2d(tf.nn.relu(d7),
+                [self.batch_size, int(s[0]), int(s[1]), self.output_c_dim], name='g_d8', with_w=True)
+            # d8 is (256 x 256 x output_c_dim)
 
-        return tf.nn.tanh(self.d8)
+            return tf.nn.tanh(self.d8)
 
     def save(self, checkpoint_dir, step):
         model_name = "pix2pix.model"
